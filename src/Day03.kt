21f4964d4417833276593bffd3997ac74ce52eee
Stephen Bain <swbain@gmail.com>
2025-12-03 19:39:46 -0600

    cleanup

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,38 +1,36 @@
+import kotlin.system.measureTimeMillis
+
 fun main() {
 
-fun String.joltage(): Long {
-    val max = dropLast(1).maxOf { it.digitToInt() }.digitToChar()
-    val index = indexOfFirst { it == max }
-    val sublist = drop(index + 1)
-    val next = sublist.maxOf { it.digitToInt() }
-    return "${max}${next}".toLong()
-}
-
-fun part1(input: List<String>): Long {
-    return input.sumOf { it.joltage() }
-}
-
-fun String.joltage2(): Long {
-    var answer = ""
-    var line = this
-    (0..11).reversed().forEach { i ->
-        val max = line.dropLast(i).maxOf { it.digitToInt() }.digitToChar()
-        val index = line.indexOfFirst { it == max }
-        line = line.drop(index + 1)
-        answer += max
+    fun String.joltage(size: Int): Long {
+        var jolts = ""
+        var batteries = this
+        for (i in size - 1 downTo 0) {
+            val max = batteries.dropLast(i).maxOf { it.digitToInt() }.digitToChar()
+            val index = batteries.indexOfFirst { it == max }
+            batteries = batteries.drop(index + 1)
+            jolts += max
+        }
+        return jolts.toLong()
     }
-    return answer.toLong()
-}
 
-fun part2(input: List<String>): Long {
-    return input.sumOf { it.joltage2() }
-}
+    fun part1(input: List<String>): Long {
+        return input.sumOf { it.joltage(size = 2) }
+    }
+
+
+    fun part2(input: List<String>): Long {
+        return input.sumOf { it.joltage(size = 12) }
+    }
 
     val testInput = readInput("Day03_test")
-//    check(part1(testInput) == 357L)
+    check(part1(testInput) == 357L)
     check(part2(testInput) == 3121910778619L)
 
     val input = readInput("Day03")
     part1(input).println()
-    part2(input).println()
+    val time = measureTimeMillis {
+        part2(input).println()
+    }
+    println("part 2 took $time ms")
 }
