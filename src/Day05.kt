415493607983571d4b47a6683165ef13bab961b6
Stephen Bain <swbain@gmail.com>
2025-12-07 22:58:37 -0600

    day5part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -15,14 +15,55 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun isOverlap(left: Pair<Long, Long>, right: Pair<Long, Long>): Boolean {
+        return when {
+            left == right -> false
+            left.first <= right.first -> left.second >= right.first
+            else -> left.first <= right.second
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val split = input.indexOfFirst { it.isBlank() }
+        val rangesIn = input.take(split)
+
+        val rangeSet = rangesIn.map {
+            val split = it.split('-')
+            split.first().trim().toLong() to split.last().trim().toLong()
+        }.toSet()
+
+        var hasDupes = true
+        var finalRanges = mutableSetOf<Pair<Long, Long>>()
+        var ranges = rangeSet
+        while (hasDupes) {
+            ranges.forEach { left ->
+                var added = false
+                val newRanges = finalRanges.toMutableSet()
+                finalRanges.forEach { right ->
+                    if (isOverlap(left, right)) {
+                        newRanges.remove(left)
+                        newRanges.remove(right)
+                        newRanges.add(minOf(left.first, right.first) to maxOf(left.second, right.second))
+                        added = true
+                        return@forEach
+                    }
+                }
+
+                if (!added) newRanges.add(left)
+
+                finalRanges = newRanges
+            }
+
+            ranges = finalRanges
+            hasDupes = ranges.any { left -> finalRanges.any { right -> isOverlap(left, right) } }
+        }
+
+        return finalRanges.sumOf { it.second - it.first + 1 }
     }
 
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 3)
-//    part2(testInput).println()
-//    check(part2(testInput) == 14)
+    check(part2(testInput) == 14L)
 
     val input = readInput("Day05")
     part1(input).println()
