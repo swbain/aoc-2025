3c0e11731026ebf1660f4d0c81dc79f76439f961
Stephen Bain <swbain@gmail.com>
2025-12-09 17:18:17 -0600

    day7part1

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,6 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var output = ""
+        var splits = 0
+        input.forEachIndexed { i, value ->
+            if (i == 0) output = value.replace('S', '|')
+            else {
+                val currentLine = value.toCharArray()
+                for (index in 0..<currentLine.count()) {
+                    val previousChar = output[index]
+                    val currentChar = currentLine[index]
+                    if (previousChar == '|') {
+                        if (currentChar == '^') {
+                            currentLine[index - 1] = '|'
+                            currentLine[index] = '.'
+                            currentLine[index + 1] = '|'
+                            splits++
+                        } else {
+                            currentLine[index] = '|'
+                        }
+                    } else {
+                        if (currentLine[index] != '|') currentLine[index] = '.'
+                    }
+                }
+                output = currentLine.concatToString()
+            }
+        }
+        return splits
     }
 
     fun part2(input: List<String>): Int {
@@ -8,7 +33,8 @@ fun main() {
     }
 
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 21)
+    check(part2(testInput) == 40)
 
     val input = readInput("Day07")
     part1(input).println()
