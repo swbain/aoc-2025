6bbef50d700591ade8a35d10f27dd1ba50c78c85
Stephen Bain <swbain@gmail.com>
2025-12-10 16:38:48 -0600

    day7part2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,3 +1,6 @@
+import utils.Grid
+import utils.Pos
+
 fun main() {
     fun part1(input: List<String>): Int {
         var output = ""
@@ -28,13 +31,27 @@ fun main() {
         return splits
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    val splits = setOf(Pos.LEFT, Pos.RIGHT)
+
+    fun solution(grid: Grid, pos: Pos, cache: HashMap<Pos, Long>): Long {
+        return when {
+            !grid.contains(pos) -> 1L
+            grid[pos].value == '^' -> splits.sumOf { cache.getOrPut(pos + it) { solution(grid, pos + it, cache) } }
+            else -> cache.getOrPut(pos + Pos.DOWN) { solution(grid, pos + Pos.DOWN, cache) }
+        }
     }
+
+    fun part2(input: List<String>): Long {
+        val grid = Grid(input)
+        val start = grid.data.flatten().first { it.value == 'S' }.pos
+        return solution(grid, start, hashMapOf())
+    }
+
+
 
     val testInput = readInput("Day07_test")
     check(part1(testInput) == 21)
-    check(part2(testInput) == 40)
+    check(part2(testInput) == 40L)
 
     val input = readInput("Day07")
     part1(input).println()
